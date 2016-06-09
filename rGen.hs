a129c3e1b8005bf628ded5da0ff06c9bd0c19709
Kjetil Flovild-Midtlie <kfm@docstream.no>
2016-06-09 20:51:39 +0200

    2nums.1=

--- rGen.hs
+++ rGen.hs
@@ -3,12 +3,16 @@ import Data.List
 
 main = do
   -- getArgs ? Name , cash, escorts, time ...
-  gen <- getStdGen
-  putStr $ intsToLines $ randInts gen 36
+  genA <- getStdGen
+  genB <- newStdGen
+  putStr $ show $ take 100 $ zip ( randInts genA 2 ) ( randInts genB 1 )
+  -- putStr $ intsToLines $ randInts gen 3
+
+joinedInts :: (Int,Int) -> String
+joinedInts (a,b) = show a ++ " " ++ show b
 
 intsToLines :: [Int] -> [Char]
 intsToLines = concat . intersperse "\n" . map show 
-
 
 randInts :: StdGen -> Int -> [Int]
 randInts g maxInt = 
